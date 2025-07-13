e4e60a7d9b62121bceb9f9436f93972ca7cfd595
ioanursut <johan.olsson82@gmail.com>
2025-07-13 14:15:08 +0000

    fixar next.config.ts för vercel

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,17 @@ import type { NextConfig } from "next";
 import { env } from "process";
 import withPWA from "next-pwa";
 
-// 👇 lägg till PWA-konfiguration
 const withPwaConfig = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: env.NODE_ENV === "development", // PWA off i dev
+  disable: env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
-  // Din tidigare config
-  allowedDevOrigins: [env.REPLIT_DOMAINS.split(",")[0]],
+  allowedDevOrigins: env.REPLIT_DOMAINS
+    ? [env.REPLIT_DOMAINS.split(",")[0]]
+    : [],
 };
 
-// 👇 exportera med PWA wrapper
 export default withPwaConfig(nextConfig);
